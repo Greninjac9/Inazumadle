1248989d53757088351f7e6e83516077d3dca3a5
Greninjac9 <leonotsuperhero@gmail.com>
2024-10-22 12:10:23 +0200

    Update extra.py

--- extra.py
+++ extra.py
@@ -1,83 +1,65 @@
 import flet as ft
-import time
-import sys
-import os
-
-def restart_program():
-    time.sleep(2)
-    python = sys.executable
-    os.execl(python, python, * sys.argv)
-
-def get_resource_path(relative_path):
-    try:
-        # PyInstaller almacena archivos de datos en una carpeta temporal
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
-
-    return os.path.join(base_path, relative_path)
-
 Table = ft.Row(
           alignment=ft.MainAxisAlignment.CENTER,
           spacing = 15,
           controls=[
               ft.Container(
-                  content=ft.Text("Jugador", weight=ft.FontWeight.BOLD),
+                  content=ft.Text("Jugador", weight=ft.FontWeight.BOLD, color=ft.colors.BLACK),
                   height = 30, width = 80,
                   bgcolor=ft.colors.GREY_300, border_radius = 10,
                   alignment=ft.alignment.center
               ),
               ft.Container(
-                  content=ft.Text("Curso", weight=ft.FontWeight.BOLD),
+                  content=ft.Text("Curso", weight=ft.FontWeight.BOLD, color=ft.colors.BLACK),
                   border = ft.border.all(5, ft.colors.GREY_300),
                   height = 30, width = 80,
                   bgcolor=ft.colors.GREY_300, border_radius = 10,
                   alignment=ft.alignment.center
               ),
               ft.Container(
-                  content=ft.Text("Elemento", weight=ft.FontWeight.BOLD),
+                  content=ft.Text("Elemento", weight=ft.FontWeight.BOLD, color=ft.colors.BLACK),
                   border = ft.border.all(5, ft.colors.GREY_300),
                   height = 30, width = 80,
                   bgcolor=ft.colors.GREY_300, border_radius = 10,
                   alignment=ft.alignment.center
               ),
               ft.Container(
-                  content=ft.Text("Posición"),
+                  content=ft.Text("Posición", weight=ft.FontWeight.BOLD, color=ft.colors.BLACK),
                   border = ft.border.all(5, ft.colors.GREY_300),
                   height = 30, width = 80,
                   bgcolor=ft.colors.GREY_300, border_radius = 10,
                   alignment=ft.alignment.center
               ),
               ft.Container(
-                  content=ft.Text("Género", weight=ft.FontWeight.BOLD),
+                  content=ft.Text("Género", weight=ft.FontWeight.BOLD, color=ft.colors.BLACK),
                   border = ft.border.all(5, ft.colors.GREY_300),
                   height = 30, width = 80,
                   bgcolor=ft.colors.GREY_300, border_radius = 10,
                   alignment=ft.alignment.center
               ),
               ft.Container(
-                  content=ft.Text("Invocador", weight=ft.FontWeight.BOLD),
+                  content=ft.Text("Invocador", weight=ft.FontWeight.BOLD, color=ft.colors.BLACK),
                   border = ft.border.all(5, ft.colors.GREY_300),
                   height = 30, width = 80,
                   bgcolor=ft.colors.GREY_300, border_radius = 10,
                   alignment=ft.alignment.center
               ),
               ft.Container(
-                  content=ft.Text("Nación", weight=ft.FontWeight.BOLD),
+                  content=ft.Text("Nación", weight=ft.FontWeight.BOLD, color=ft.colors.BLACK),
                   border = ft.border.all(5, ft.colors.GREY_300),
                   height = 30, width = 80,
                   bgcolor=ft.colors.GREY_300, border_radius = 10,
                   alignment=ft.alignment.center
               ),
               ft.Container(
-                  content=ft.Text("Debut", weight=ft.FontWeight.BOLD),
+                  content=ft.Text("Debut", weight=ft.FontWeight.BOLD, color=ft.colors.BLACK),
                   border = ft.border.all(5, ft.colors.GREY_300),
                   height = 30, width = 80,
                   bgcolor=ft.colors.GREY_300, border_radius = 10,
                   alignment=ft.alignment.center
               ),
               ft.Container(
-                  content=ft.Text("Equipo", weight=ft.FontWeight.BOLD),
+                  content=ft.Text("Equipo", weight=ft.FontWeight.BOLD, color=ft.colors.BLACK),
                   border = ft.border.all(5, ft.colors.GREY_300),
                   height = 30, width = 80,
                   bgcolor=ft.colors.GREY_300, border_radius = 10,
